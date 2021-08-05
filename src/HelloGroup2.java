ccff4c6c1e414d738ea290345b1d61e9551fbb8e
Ateequllah Saraabi <ateequllah.saraabi@gmail.com>
2021-08-05 11:52:21 -0700

    created a print line 2

--- src/HelloGroup2.java
+++ src/HelloGroup2.java
@@ -2,5 +2,6 @@ public class HelloGroup2 {
     public static void main(String[] args) {
 
         System.out.println("Creating new printline for gitHub work flow practice");
+        System.out.println("Creating second print line for gitHub work flow practice");
     }
 }
