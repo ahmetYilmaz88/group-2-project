1cbecf3817130defe0b1c2b8995b016155a57b47
ahmadsam33r <86983918+ahmadsam33r@users.noreply.github.com>
2021-08-05 12:39:11 -0700

    start working to buy the tesla

--- src/samir/car.java
+++ src/samir/car.java
@@ -4,6 +4,8 @@ public class car {
     public static void main(String[] args) {
 
         System.out.println("I love Tesla but done have money ");
+
+        // i wish yilmaz tell me a stock to buy and make money out of that .
     }
 
 }
