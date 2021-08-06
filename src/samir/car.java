56d185457380860e2268eb3cd35b165e4a3a8c8e
ahmadsam33r <86983918+ahmadsam33r@users.noreply.github.com>
2021-08-06 08:21:16 -0700

    start working on car @friday

--- src/samir/car.java
+++ src/samir/car.java
@@ -8,6 +8,9 @@ public class car {
         // i wish yilmaz tell me a stock to buy and make money out of that .
         // still working
 
+        // today friday time 8:20 am
+        // still trying working on this project
+
     }
 
 
