e80421671004b67f06e071b9f797d86c90a3e91e
Ateequllah Saraabi <ateequllah.saraabi@gmail.com>
2021-08-06 07:48:27 -0700

    added a new branch and a class

---
+++ src/Ateeq/LogoutFunction.java
@@ -0,0 +1,7 @@
+package Ateeq;
+
+public class LogoutFunction {
+    public static void main(String[] args) {
+        System.out.println("Added a new class and branch under my folder name");
+    }
+}
